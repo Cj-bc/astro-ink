4918546bd65e5917eb5fa5c9aee01b769fa2731e
Cj-bc <cj.bc-sd@outlook.jp>
2024-05-18 17:33:14 +0900

    Change: src/content/config> Transform date property to support org-mode timestamp

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,11 @@ const blogCollection = defineCollection({
         description: z.string(),
         tags: z.string(),
         author: z.string(),
-        date: z.string(),
+        date: z.string().transform((str) =>
+	    {
+		let cap = str.match(/\[(\d{4}-\d{2}-\d{2}) ...(?: (\d{2}:\d{2}))?\]/)
+		return `${cap[1]} ${cap[2] ?? ""}`
+	    }),
         image: z.string().optional(),
         category: z.string(),
     })
