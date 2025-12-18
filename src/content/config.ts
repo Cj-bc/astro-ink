48e87851c295e102cfa19d255ce07fa36b263a90
Cj-bc <cj.bc-sd@outlook.jp>
2025-12-18 10:02:16 +0900

    Refractor: use spaces only for indent

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,18 +5,18 @@ const blogCollection = defineCollection({
         title: z.string().max(100, 'The title length must be less than or equal to 100 chars'),
         description: z.string().default("read more..."),
         tags: z.string()
-	    .transform((str) => str.split(":").filter((s) => s != '' && s != " "))
-	    .pipe(z.array(z.string())),
+               .transform((str) => str.split(":").filter((s) => s != '' && s != " "))
+                .pipe(z.array(z.string())),
         author: z.string().default("Cj-bc"),
         date: z.string().transform((str) =>
-	    {
-		let cap = str.match(/\[(\d{4}-\d{2}-\d{2}) ...(?: (\d{2}:\d{2}))?\]/)
-		return `${cap[1]} ${cap[2] ?? ""}`
-	    }),
+        {
+            let cap = str.match(/\[(\d{4}-\d{2}-\d{2}) ...(?: (\d{2}:\d{2}))?\]/)
+            return `${cap[1]} ${cap[2] ?? ""}`
+        }),
         image: z.string().optional(),
         kind: z.enum(["Memo", "Diary", "Knowledge", "Advertisment", "Translation", "HowTo"]),
-	progress: z.enum(["Empty", "WIP", "Published"]),
-	status: z.enum(["Normal", "Archive", "Accuracy", "Outdated"])
+        progress: z.enum(["Empty", "WIP", "Published"]),
+        status: z.enum(["Normal", "Archive", "Accuracy", "Outdated"])
     })
 })
 
