cc60c882bef73b4bfa4e5d4cabf934be6a5923cc
Cj-bc <cj.bc-sd@outlook.jp>
2026-01-01 02:07:50 +0900

    fix: snippets were not loaded

--- src/components/ModeSensitive.svelte
+++ src/components/ModeSensitive.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
     import { theme } from '../store/theme'
+    import type { Snippet } from 'svelte';
+    type Props = {
+	dark: Snippet;
+	light: Snippet;
+    }
+
+    let { dark, light }: Props = $props();
 </script>
 {#if $theme === 'dark'}
      {@render dark()}
